7306ecbdfbf8e3644e71f1bd499366083e5a4659
Ziyang Wang <zw2425@ic.ac.uk>
2025-10-06 15:52:53 +0100

    Initial commit

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Author: Your Name your.login@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2019
+
+chmod +x boilerplate.sh
+./boilerplate.sh apple banana
