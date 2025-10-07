ffaa93906a0f8bc8758182a341fb31bf8ce8c280
Ziyang Wang <zw2425@ic.ac.uk>
2025-10-07 13:21:54 +0100

    pop up error if no arguments provided #2

--- week1/code/ConcatenateTwoFiles.sh
+++ week1/code/ConcatenateTwoFiles.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#If 3 arguments are not provided, print this 'error' message
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <file1> <file2> <output_file>"
+    exit 1
+fi
+
 cat $1 > $3
 cat $2 >> $3
 echo "Merged File is"
--- week1/code/tabtocsv.sh
+++ week1/code/tabtocsv.sh
@@ -12,9 +12,11 @@
 # Check that exactly one argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Error: Missing input file."
+    echo "Usage: $0 <input_file>"
     exit 1
 fi
 
+# Cat the input file, replace tab with comma, save to a new file with .csv extension
 echo "Creating a comma delimited version of $1 ..."
 cat $1 | tr -s "\t" "," >> $1.csv
 echo "Done!"
